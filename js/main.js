1040d61066357e661ba627c53b53886625fe8600
marcoteixeirab15 <marco.teixeirab15@gmail.com>
2021-10-06 23:28:33 -0300

    Adiciona novo registro a tabela

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function setList(list){
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
 
     for(var key in list){
-        table += '<tr><td>' + formatDesc(list[key].desc) + '</td><td>' + list[key].amount + '</td><td>' + formatValue(list[key].value) + '</td><td>Edit | Delete</td></tr>'
+        table += '<tr><td>' + formatDesc(list[key].desc) + '</td><td>' + list[key].amount + '</td><td>' + formatValue(list[key].value) + '</td><td> <button class="btn btn-success" onclick="setUpdate(' + key + ');"> Edit </button>| Delete</td></tr>'
     }
 
     table += '</tbody>'
@@ -34,6 +34,34 @@ function formatValue(value){
     str = str.replace(".", ",");
     str = "R$ " + str;
     return str;
+}
+
+function addData(){
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+
+    list.unshift({"desc": desc, "amount": amount, "value": value});
+
+    setList(list);
+}
+
+function setUpdate(id){
+    var obj = list[id];
+
+    document.getElementById("desc").value = obj.desc;
+    document.getElementById("amount").value = obj.amount;
+    document.getElementById("value").value = obj.value;
+    document.getElementById("btnUpdate").style.display = "inline-block";
+    document.getElementById("btnAdd").style.display = "none";
+}
+
+function resetForm(){
+    document.getElementById("desc").value = "";
+    document.getElementById("amount").value = "";
+    document.getElementById("value").value = "";
+    document.getElementById("btnUpdate").style.display = "none";
+    document.getElementById("btnAdd").style.display = "inline-block";
 }
 
 setList(list);
